91979723d25e71ff9dd59e81c69bdfef59a15e54
LoGo124 <nacholopezgomez2002@gmail.com>
2024-07-22 08:38:30 +0200

    returnt to 1m

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ coins = {"BTC":"Bitcoin"}#,"ETH":"Ethereum"}
 if __name__ == "__main__":
     broker = Alpaca(ALPACA_CONFIG)
     prods = [CryptoCurrency(coins[coin], coin) for coin in coins]
-    preds = [Mosley(product=prod, interval="2m", changepoint_prior_scale=0.5, changepoint_range=0.8, show_mode="term") for prod in prods]
-    strategies = [SwingWithPred(broker=broker, max_cash=1000, product=pred.product, predictor=pred, sleeptime="2M") for pred in preds]
+    preds = [Mosley(product=prod, interval="1m", changepoint_prior_scale=0.5, changepoint_range=0.8, show_mode="term") for prod in prods]
+    strategies = [SwingWithPred(broker=broker, max_cash=1000, product=pred.product, predictor=pred, sleeptime="1M") for pred in preds]
     trader = Trader(strategies=strategies, logfile="logs/traders/log_test_01.log",debug=False)
     trader._set_logger()
     trader.run_all()
