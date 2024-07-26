358674e2374bcc93a12100dad2eb0c3608535638
LoGo124 <nacholopezgomez2002@gmail.com>
2024-07-26 22:11:00 +0200

    logfile name with datetime

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import sys
+import datetime
 
-from lumibot.entities import Asset
 from config import ALPACA_CONFIG
 from lumibot.brokers import Alpaca
-from lumibot.strategies import Strategy
 from lumibot.traders import Trader
 
 from src.classes.Strategies import SwingWithPred
@@ -17,7 +15,7 @@ if __name__ == "__main__":
     prods = [CryptoCurrency(coins[coin], coin) for coin in coins]
     preds = [Mosley(product=prod, interval="1m", changepoint_prior_scale=0.5, changepoint_range=0.8, show_mode="term") for prod in prods]
     strategies = [SwingWithPred(broker=broker, max_cash=1000, product=pred.product, predictor=pred, sleeptime="1M") for pred in preds]
-    trader = Trader(strategies=strategies, logfile="logs/traders/log_test_01.log",debug=False)
+    trader = Trader(strategies=strategies, logfile=f"logs/traders/log_test_{datetime.now()}.log",debug=False)
     trader._set_logger()
     trader.run_all()
     #input("\n------STOP------\n")
