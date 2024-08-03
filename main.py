14f1ab237954e28a0bc09583d27b9ea1ddfee83d
LoGo124 <nacholopezgomez2002@gmail.com>
2024-08-03 18:03:21 +0200

    AI brain adjust

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ coins = {"BTC":"Bitcoin"}#,"ETH":"Ethereum"}
 if __name__ == "__main__":
     broker = Alpaca(ALPACA_CONFIG)
     prods = [CryptoCurrency(coins[coin], coin) for coin in coins]
-    preds = [Mosley(product=prod, interval="1m", changepoint_prior_scale=0.5, changepoint_range=0.8, show_mode="term") for prod in prods]
+    preds = [Mosley(product=prod, interval="1m", changepoint_prior_scale=1, changepoint_range=0.99, show_mode="term") for prod in prods]
     strategies = [SwingWithPred(broker=broker, max_cash=1000, product=pred.product, predictor=pred, sleeptime="1M") for pred in preds]
     trader = Trader(strategies=strategies, logfile=f"logs/traders/log_test_{datetime.now()}.log",debug=False)
     trader._set_logger()
